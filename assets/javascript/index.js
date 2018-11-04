ce6cfc3167c3112cc2ef2a0635435624d549fc8a
Michelle Brewington <michelle.brewington@ge.com>
2018-11-04 18:57:15 -0500

    Final

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,30 +1,96 @@
+var count = 0;
+
 // Grocery List array
-var categories=[];
-var products = $('.product');
-for(let i=0; i <products.length; i++){
-  $(`#${products[i].id}`).on('click', function(){
-    $('#shopping-cart').append(`<li><a href="#">${products[i].text}</a></li>`);
-    console.log(products[i].id);
-  });
+let productList = [
+  {
+    name: 'Lobster',
+    category: 'Seafood'
+  },
+
+  {
+    name: 'T-Bone Steaks',
+    category: 'Meat'
+  },
+
+  {
+    name: 'Baked Potatoes',
+    category: 'produce'
+  },
+
+  {
+    name: 'Asparagus',
+    category: 'veggie'
+  },
+
+  {
+    name: 'Parmesean Cheese',
+    category: 'dairy'
+  },
+
+  {
+    name: 'Butter',
+    category: 'dairy'
+  },
+
+  {
+    name: 'Garlic Bread',
+    category: 'bread'
+  },
+
+  {
+    name: 'Shredded Cheese',
+    category: 'dairy'
+  },
+
+  {
+    name: 'Sour Cream',
+    category: 'dairy'
+  },
+
+  {
+    name: 'Strawberry Cheesecake',
+    category: 'dessert'
+  }
+
+];
+
+const render = function () {
+  var products = $('.Product');
+  products.empty()
+  for (let i = 0; i < items.length; i++) {
+    console.log(items[i].name)
+    products.append('<div>' + items[i].name + '</div>');
+  };
 }
+// render()
+
 
 // Recieve the name and index as parameters.
-function duplicateCheck(name, index){
+function duplicateCheck(name, index) {
   // Get all items that are currently in the cart.
-  var cartList = $('.cart-list'), item = $(`#btn-${index}`);
+  var cartList = $('.cart-list');
+  var item = $(`#btn-${index}`);
+
+  console.log(cartList);
   // For each item in the cart list, check to see if the given name matches.
-  for(let i = 0; i < cartList.length; i++){
-      if(item.text() === name){
-          // Alert the user if it already exists.
-          alert(name + ' already exists in cart!');
-      }else{
-          // Create the button and event listener for the button that's in the shopping cart.
-          cartList.append(`<button id="btn-${index}" class="cart m-2 btn-${index} btn-outline-danger">${name}</button>`);
-          $(`#btn-${index}`).on('click', function(){
-              // Removes the button from the shopping cart.
-              $(`#btn-${index}`).remove();
-          });
-      }
+  for (let i = 0; i < cartList.length; i++) {
+    if (item.text() === name) {
+      // Alert the user if it already exists.
+      alert(name + ' already exists in cart!');
+    } else {
+      count++;
+      $('#totalItems').text(count)
+      $('.product-quantity').text(count);
+      // Create the button and event listener for the button that's in the shopping cart.
+      cartList.append(`<button id="btn-${index}" class="cart m-2 btn-${index} btn-outline-success">${name}</button>`);
+      $(`#btn-${index}`).on('click', function () {
+        count--;
+        $('#totalItems').text(count)
+        $('.product-quantity').text(count);
+        // Removes the button from the shopping cart.
+        $(`#btn-${index}`).remove();
+      });
+    }
   }
 }
 
@@ -33,43 +99,17 @@ for (let i = 0; i < productList.length; i++) {
   $('.item-list').append(`<button class="product m-2 btn-${i} ${productList[i].category}">${productList[i].name}</button>`);
   // Create the event listener that checks the shopping cart for duplicates.
   $(`.btn-${i}`).on('click', function () {
-      duplicateCheck(productList[i].name, i);
+    duplicateCheck(productList[i].name, i);
   });
 }
 
-// For each item in productList, dynamically populate categories array.
-productList.forEach(item => {
-  let exists = false;
-
-  // For each category that is in the array, check to see if the item's
-  // category already exists in the categories array.
-  categories.forEach(category => {
-      if(item.category === category){exists = true;}
-  });
-  // If it doesn't exist, push the new category to the categories array.
-  if(!exists){
-      categories.push(item.category);
-      // Create the button for the new category.
-      $('.categories').append(`<button class="m-2" id="${item.category}">${item.category}</button>`);
-
-      // Create the event listener for the category button.
-      $(`#${item.category}`).on('click', function(){
-          // Hide all products.
-          $('.product').hide();
-          // Only show the products that have the class that matches the category button.
-          $(`.${item.category}`).show();
-      });
-      exists = false;
-  }
-});
-
 // When all gets clicked, show all products button
-$('#all').on('click', function(){
+$('#all').on('click', function () {
   $('.product').show();
 });
 
 
 
-$('#totalItems').text(items)
-$('.totalAmount').text('$ '+total+ ' USD')
+$('#totalItems').text(count)
+// $('.totalAmount').text('$ ' + total + ' USD')
 
